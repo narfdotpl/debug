e10e7fda2fba256627aabc3c6aca1410a96f2711
Maciej Konieczny <hello@narf.pl>
2011-05-08 19:01:31 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from distutils.core import setup
+
+
+setup(
+    name='debug',
+    version='0.1.0dev',
+    description='Start fancy debugger in a single statement.',
+    author='Maciej Konieczny',
+    author_email='hello@narf.pl',
+    url='https://github.com/narfdotpl/debug',
+    py_modules=['debug'],
+    install_requires=['ipython >= 0.10, < 0.11', 'see >= 1.0']
+)
