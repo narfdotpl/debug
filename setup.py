f19397ed7afdf9f7ee72cbccabd75e4799c26df1
Maciej Konieczny <hello@narf.pl>
2015-03-31 10:33:44 +0200

    #6: lowered ipdb requirement to the highest released version

--- setup.py
+++ setup.py
@@ -12,5 +12,5 @@ setup(
     author_email='hello@narf.pl',
     url='https://github.com/narfdotpl/debug',
     py_modules=['debug'],
-    install_requires=['ipdb >= 0.8.1', 'see >= 1.1']
+    install_requires=['ipdb >= 0.8', 'see >= 1.1']
 )
