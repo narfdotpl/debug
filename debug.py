5b14c4ab7bd61fbe3958e27b30b0d6c09413463a
Maciej Konieczny <hello@narf.pl>
2016-10-26 19:35:23 +0200

    fixed `level` logic

--- debug.py
+++ debug.py
@@ -34,13 +34,34 @@ debug()
 
 # monkeypatch `import` so `import debug` will work every time
 def new_import(*args, **kwargs):
+    # don't touch non-debug imports
     name = args[0]
-    level = args[-1]
-    is_absolute = level is None or level == 0
-
-    if name == 'debug' and is_absolute:
-        debug()
-    else:
+    if name != 'debug':
         return old_import(*args, **kwargs)
+
+    # Make sure that the name "debug" refers to *this* module,
+    # i.e. that the import is `import debug` and not for example
+    # `from .debug import foo`.
+    #
+    # To check this I have to look at the value of the `level`
+    # argument.  It's the last argument and it has a default value.
+    # I don't want to add it to the signature of `new_import` because
+    # `__import__` is different in Python 2 and 3:
+    #
+    #     __import__(name, globals={}, locals={}, fromlist=[], level=-1)
+    #     __import__(name, globals=None, locals=None, fromlist=(), level=0)
+    #
+    # I work around this by getting `level` from `kwargs` and `args`,
+    # which works the same way in both Python versions.
+    is_relative = False
+    if 'level' in kwargs:
+        is_relative = kwargs['level'] > 0
+    elif len(args) == 5:
+        is_relative = args[-1] > 0
+
+    if is_relative:
+        return old_import(*args, **kwargs)
+    else:
+        debug()
 
 __builtin__.__import__ = new_import
